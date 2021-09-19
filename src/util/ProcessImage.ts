319bfd5cc9a60dd8c426622b0eca3f0307ccc1ff
Ural Bayhan <uralbayhan0@gmail.com>
2021-09-19 02:18:12 +0200

    Remove debug console.log

--- src/util/ProcessImage.ts
+++ src/util/ProcessImage.ts
@@ -4,9 +4,6 @@ const processImageUrl = (imageUrl: string) => {
     .replace(/(w(idth)?=)\d+/, `$1${Math.floor(15 * em)}`)
     .replace(/ops=\d+_\d+/, `ops=${em * 15}_${em * 9}`)
     .replace(/(https:\/\/storage.googleapis.com\/.*)\/\d+.(jpeg|png|jpg)/, `$1/200.$2`);
-  if (imageUrl !== newUrl) {
-    console.log('newurl', newUrl);
-  }
   return newUrl;
 };
 
