f150e3257ddef824c46ff8c0d249a766f3e3db87
Ural Bayhan <uralbayhan0@gmail.com>
2021-09-19 02:17:20 +0200

    Adjust height parameter of cdn image links

--- src/util/ProcessImage.ts
+++ src/util/ProcessImage.ts
@@ -2,7 +2,7 @@ const processImageUrl = (imageUrl: string) => {
   const em = parseFloat(getComputedStyle(document.documentElement).fontSize);
   const newUrl = imageUrl
     .replace(/(w(idth)?=)\d+/, `$1${Math.floor(15 * em)}`)
-    .replace(/ops=\d+_\d+/, `ops=${em * 15}_${em * 6}`)
+    .replace(/ops=\d+_\d+/, `ops=${em * 15}_${em * 9}`)
     .replace(/(https:\/\/storage.googleapis.com\/.*)\/\d+.(jpeg|png|jpg)/, `$1/200.$2`);
   if (imageUrl !== newUrl) {
     console.log('newurl', newUrl);
